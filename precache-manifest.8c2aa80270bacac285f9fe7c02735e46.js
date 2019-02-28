f09dc156c037d6e6c19e4947d43593146b50d2da
dFegnoux <denis.fegnoux@gmail.com>
2019-02-28 13:18:19 +0100

    0.0.1

---
+++ precache-manifest.8c2aa80270bacac285f9fe7c02735e46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4cb36c1946b7c8cf2231",
+    "url": "/static/js/main.4cb36c19.chunk.js"
+  },
+  {
+    "revision": "5a5df9bd8df5f47a4f63",
+    "url": "/static/js/2.5a5df9bd.chunk.js"
+  },
+  {
+    "revision": "4cb36c1946b7c8cf2231",
+    "url": "/static/css/main.06491828.chunk.css"
+  },
+  {
+    "revision": "e3e6de75e0863f943aa6a91a1bba1182",
+    "url": "/index.html"
+  }
+];
