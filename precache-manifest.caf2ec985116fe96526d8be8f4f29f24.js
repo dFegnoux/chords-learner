422e85c31bfe6b74b63d110ccf9981b060821b86
dFegnoux <denis.fegnoux@gmail.com>
2019-02-28 15:01:44 +0100

    Updates

---
+++ precache-manifest.caf2ec985116fe96526d8be8f4f29f24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f5ef1e603ea4b0c0eda",
+    "url": "/chords-learner/static/js/runtime~main.7f5ef1e6.js"
+  },
+  {
+    "revision": "de047be52dc26d8e442a",
+    "url": "/chords-learner/static/js/main.de047be5.chunk.js"
+  },
+  {
+    "revision": "5a5df9bd8df5f47a4f63",
+    "url": "/chords-learner/static/js/2.5a5df9bd.chunk.js"
+  },
+  {
+    "revision": "de047be52dc26d8e442a",
+    "url": "/chords-learner/static/css/main.06491828.chunk.css"
+  },
+  {
+    "revision": "d30bfa0a267f3ba78e30a8031e63247f",
+    "url": "/chords-learner/index.html"
+  }
+];
